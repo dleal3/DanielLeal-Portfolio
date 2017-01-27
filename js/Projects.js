80234df3cc3f78c575fb837f91ed226af889f06f
Daniel <dray364@gmail.com>
2017-01-27 09:36:20 -0600

    adding unstagged commits

--- js/Projects.js
+++ js/Projects.js
@@ -5,7 +5,7 @@ export default React.createClass({
   render() {
     return (
       <section>
-        <h1> Projects would go here </h1>
+        <h1>  </h1>
 
         <nav>
           <a href="file:///Users/danielleal/assignments/assignments/2.4-dom-manipulation/index.html"> Javascript Calculator </a>
