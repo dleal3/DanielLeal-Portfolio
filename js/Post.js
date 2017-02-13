115e6f8de9d018e6d3f17d09f6d01eb2363b4b1e
Daniel <dray364@gmail.com>
2017-02-12 21:24:18 -0600

    working on blog post page by adding new photos to link to my blog page

--- js/Post.js
+++ js/Post.js
@@ -7,8 +7,8 @@ export default React.createClass({
       <section>
         <nav className="blogNav"><a href="https://medium.com/@dleal3" className="blogNav">Click for More</a></nav>
 
-        <a href="https://medium.com/@dleal3/the-only-way-to-make-progress-is-to-get-lost-in-the-process-183cd93d7e9b#.4vsvgk402">
-          <img src="styles/image_for_blog.png" alt="" className="blogImage"/></a>
+        <a href="https://medium.com/@dleal3/1-1-unbreak-the-internet-a80425ccb65f#.ub66cz7bd">
+          <img src="styles/unbreak-the-internet.png" alt="" className="blogImage"/></a>
         <a href="https://medium.com/@dleal3/time-away-from-the-usual-767601787f79#.u88q37hb8">
           <img src="styles/timeaway.png" alt="" className="blogImage"/></a>
         <a href="https://medium.com/@dleal3/time-away-from-the-usual-767601787f79#.u88q37hb8">
